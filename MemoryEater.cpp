8a96ed7f9f816876ab42ceda929222a9cd4d98d4
LuckyyS0da <shouryathakur5600@gmail.com>
2024-09-14 00:44:14 +0530

    Add files via upload

---
+++ MemoryEater.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int nat[9];
+    for (int i = 0; i = 9; i ++ ){
+        nat[i] = i + 1;
+        cout << nat[i] << endl;
+    }
+
+
+}
